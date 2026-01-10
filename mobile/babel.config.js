a5f1900b64828e4449be1158f2c37137d074fd1a
Burger Vivier <burger@boksburger.co.za>
2026-01-10 06:46:30 +0200

    chore: update dependencies in mobile package.json
    - Bump react and react-dom to version 19.2.3
    - Add react-native-web as a dependency
    - Update @types/react to version 19.2.3
    - Add Babel plugins for class properties, private methods, and private properties
    - Update babel-preset-expo to version 9.5.1

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -6,6 +6,9 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: false }],
+      ['@babel/plugin-transform-private-methods', { loose: false }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: false }],
       [
         "module-resolver",
         {
